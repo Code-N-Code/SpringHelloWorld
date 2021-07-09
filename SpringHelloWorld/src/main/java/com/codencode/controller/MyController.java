067c99df96de6934884bbd418534b3f06adfa667
Code-N-Code <icodencode@gmail.com>
2021-07-09 13:10:02 +0530

    Initial commit

---
+++ SpringHelloWorld/src/main/java/com/codencode/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.codencode.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World from CodeNCode";
+    }
+}
