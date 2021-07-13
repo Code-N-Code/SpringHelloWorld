60714809f158654d0ddd9a8caeeaff422d900f53
Code-N-Code <icodencode@gmail.com>
2021-07-13 13:21:56 +0530

    added hayHelloUser endpoint

--- SpringHelloWorld/src/main/java/com/codencode/controller/MyController.java
+++ SpringHelloWorld/src/main/java/com/codencode/controller/MyController.java
@@ -1,6 +1,7 @@
 package com.codencode.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,11 @@ public class MyController {
     @GetMapping("/logout")
     public String logout(){
         return "return Logged Out";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHelloToUser(@PathVariable String name)
+    {
+        return "Hello " + name + " from CodeNCode";
     }
 }
